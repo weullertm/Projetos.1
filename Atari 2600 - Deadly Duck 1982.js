c0c4d4850379fe99e4effe953b55addbc3ad6a5d
weullertm <93833242+weullertm@users.noreply.github.com>
2022-05-24 22:20:05 -0300

    Update Atari 2600 - Deadly Duck 1982.js

--- Atari 2600 - Deadly Duck 1982.js
+++ Atari 2600 - Deadly Duck 1982.js
@@ -12,17 +12,17 @@ var ganhou = false;
 function setup() {
   createCanvas(600, 500);
   noStroke();
-    
+   
   p = new Player();
   tiro = new Tiro();
   inimigos = [];
   tiros = [];
-  
+ 
   for(var i = 0; i < N; i++) {
     inimigos.push(new Inimigos());
     tiros.push(new TirosInimigos());
   }
-  
+ 
 }
 
 function draw() {
@@ -53,28 +53,29 @@ class Player {
     this.y = 380;
     this.dx = 0;
   }
-  
+ 
   update() {
     this.x += this.dx;
-    this.x = constrain(this.x, 20, width - 20);
-    
+    this.x = constrain(this.x, 50, width - 50);
+   
     for(var i = 0; i < N; i++) {
       var d = tiros[i].dist(this.x, this.y);
-      
+     
+      //para quando encosta no tiro no chão
       if (d < 25) {
         this.dx = 0;
         this.x = this.x;
       }
     }
-    
+   
     fill(50);
     ellipse(this.x, this.y, 40, 40);
   }
-  
+ 
   mover(dx) {
     this.dx = dx;
   }
-    
+   
   dist(x, y) {
     return dist(x, y, this.x, this.y);
   }
@@ -86,21 +87,23 @@ class Tiro {
     this.y = 0;
     this.dy = 0;
   }
-  
+ 
   update() {
     this.y += this.dy;
-    
+   
     for(var i = 0; i < N; i++) {
       var d = inimigos[i].dist(this.x, this.y);
       var d2 = tiros[i].dist(this.x, this.y);
-      
+     
+      //verifica o choque entre os tiros e os anula
       if (d2 < 10) {
         this.y = p.y;
         this.dy = p.dy;
         tiros[i].y = inimigos[i].y;
         tiros[i].dy = inimigos[i].dy;
       }
-    
+     
+      //verifica o choque entre o tiro e os inimigos, conta ponto e anula inimigo atingido e seu respectivo tiro
       if (d < 17.5) {
         pontos++;
         this.y = p.y;
@@ -109,22 +112,25 @@ class Tiro {
         inimigos[i].y = 0;
         inimigos[i].dy = 0;
         inimigos[i].tamanho = 0;
+        tiros[i].x = 0;
+        tiros[i].y = 0;
+        tiros[i].dy = 0;
         tiros[i].tamanho = 0;
       }
     }
-      
-    fill(255);
+     
+    fill(255, 255, 0);
     ellipse(this.x, this.y, 10, 10);
-    
+   
     if (pontos >= 8) {
       background(220);
       fill(0);
       textSize(15);
-      text("Você ganhou! Pressione ENTER para jogar novamente!", 120, 250);
+      text("VOCÊ GANHOU! Pressione ENTER para jogar novamente!", 120, 250);
       ganhou = true;
     }
   }
-  
+ 
   atirar(nx, ny, delta) {
     this.x = nx;
     this.y = ny;
@@ -139,22 +145,24 @@ class Inimigos {
     this.dx = random(-5, 5);
     this.tamanho = 25;
   }
-  
+ 
   update() {
     this.x += this.dx;
-    
-    if (t == 60 || t == 120) {
+   
+    //troca o sentido do movimento a cada 0.5 s
+    if (t == 30 || t == 60 || t == 90 || t == 120 && this.x > 50 && this.x < 550) {
       this.dx = random(-5, 5);
     }
-    
-    if (this.x <= 50 || this.x >= 550) {
+   
+    //troca o sentido quando se aproxima das paredes
+    if (this.x < 50 || this.x > 550) {
       this.dx = -this.dx;
     }
-    
+   
     fill(100);
     ellipse(this.x, this.y, this.tamanho, this.tamanho);
   }
-  
+ 
   dist(x, y) {
     return dist(x, y, this.x, this.y);
   }
@@ -165,45 +173,63 @@ class TirosInimigos {
     this.x = 0;
     this.y = 0;
     this.dy = 0;
-    this.tamanho = 10;
+    this.tamanho = 0;
   }
-  
-  update() {
+ 
+  update() {    
     if (t > 120) {
       for(var i = 0; i < N; i++) {
-        tiros[i].atirar(inimigos[i].x, inimigos[i].y, 5);
-        t = 0;
+        //atira se o Player estiver na faixa de tiro
+        if(inimigos[i].x > p.x - 150 && inimigos[i].x < p.x + 150) {  
+          tiros[i].atirar(inimigos[i].x, inimigos[i].y, 5);
+        } else {
+          //não atira quando não estiver na faixa de tiro
+          this.tamanho = 0;
+          tiros[i].atirar(0, 0, 0)
+        }
       }
+     
+      t = 0;
     }
-    
+   
+    //tiros inimigos só na faixa de jogo
+    if (this.x > 50) {
+      this.tamanho = 10;
+    } else {
+      this.tamanho = 0;
+    }
+   
+    //verifica o choque entre os tiros e o Player
     var d = p.dist(this.x, this.y);
-    
-    if (d <= 25 && this.y < 400) {
+   
+    if (d < 25 && this.y < 400) {
       vidas--;
       this.y = inimigos.y;
     }
-    
+   
     this.y += this.dy;
 
     var t1 = 0;
+    //mantém o tiro no chão
     if (this.y > 400) {
       this.y = 400;
       t1++;
     }
-    
+   
+    //após 2 s o tiro desaparece do chão
     if (t1 > 120) {
       this.y = inimigos.y;
       t = 0;
     }
-                  
-    fill(255);
+                 
+    fill(255, 0, 0);
     ellipse(this.x, this.y, this.tamanho, this.tamanho);
-    
+   
     if (vidas <= 0) {
       background(220);
       fill(0);
       textSize(15);
-      text("Você perdeu! Pressione ENTER para recomeçar!", 150, 250);
+      text("VOCÊ PERDEU! Pressione ENTER para recomeçar!", 150, 250);
       perdeu = true;
     }
 
@@ -214,7 +240,7 @@ class TirosInimigos {
     this.y = ny;
     this.dy = delta;
   }
-  
+ 
   dist(x, y) {
     return dist(x, y, this.x, this.y);
   }
@@ -224,27 +250,27 @@ function keyPressed() {
   if (keyCode == LEFT_ARROW) {
     p.mover(-5);
   }
-  
+ 
   if (keyCode == RIGHT_ARROW) {
     p.mover(5);
   }
-  
+ 
   if (keyCode == UP_ARROW) {
     tiro.atirar(p.x, p.y, -10);
   }
-  
+ 
   if (perdeu == true || ganhou == true) {
     if (keyCode == ENTER) {
         p = new Player();
         tiro = new Tiro();
         inimigos = [];
         tiros = [];
-  
+ 
         for(var i = 0; i < N; i++) {
           inimigos.push(new Inimigos());
           tiros.push(new TirosInimigos());
         }
-      
+     
         vidas = 4;
         pontos = 0;
         perdeu = false;
